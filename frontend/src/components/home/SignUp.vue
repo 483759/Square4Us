a7efdb1bd3d128fbbf1fc0301db83f0f58f0c555
cactuswarrior <80228738+cactuswarrior@users.noreply.github.com>
2021-07-20 16:47:24 +0900

    S05P12B308-21 [feat]: 회원가입 기능 간단구현(frontend)

---
+++ frontend/src/components/home/SignUp.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <div style="height: 100px">
+      <h3>email</h3>
+      <input style="height: 40px " type="text" v-model="state.email"> 
+    </div>
+  
+  <div style="height: 100px">
+    <h3>password</h3>
+    <input style="height: 40px " type="text" v-model="state.password">
+  </div>
+
+  <div style="height: 100px">
+    <h3>password confirmation</h3>
+    <input style="height: 40px " type="text" v-model="state.password_confirmation">
+  </div>
+  
+   <!-- @click="signup" -->
+  </div>  
+  
+
+
+
+
+  
+
+</template>
+
+<script>
+// import axios from 'axios'
+import {reactive} from 'vue'
+export default {
+    name: 'SignUp',
+    setup() {
+      let state = reactive({
+        email: '',
+        password: '',
+        password_confirmation: '',
+      })
+      console.log(state)
+      // function signup() {
+      //   axios({
+      //     method: 'post',
+      //   })
+      // }
+
+      return {
+        state
+      }
+    }
+
+
+}
+</script>
+
+<style>
+
+</style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,11 +1,62 @@
 <template>
 <h1>Home</h1>
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal"  @click="signup">
+  sign up
+</button>
+
+<!-- Modal -->
+<div class="modal fade modal-dialog modal-dialog-centered  modal-dialog-scrollable" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" >
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">SignUp</h5>
+        
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <SignUp />
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" @click="signup">Close</button>
+        <button type="button" class="btn btn-primary" >Sign Up</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
 </template>
 
 <script>
+import SignUp from '@/components/home/SignUp'
+import { reactive } from '@vue/reactivity'
 export default {
   name: 'Home',
   components: {
+    SignUp,
+  },
+  setup() {
+    let state = reactive({
+      signupmodal: false,
+    })
+
+    function signup() {
+      state.signupmodal = !state.signupmodal
+      console.log(state.signupmodal)
+       }
+    return {
+      state,
+      signup,
+      
+    }
+    
   }
 }
 </script>
+<style>
+.modal-dialog.modal-bigsize { width:100%; height: 30%; margin: 0; padding: 0; }
+</style>
