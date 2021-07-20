9518b938f7860b343a6df20e4c0026e32700c7d1
wns312 <61006711+wns312@users.noreply.github.com>
2021-07-20 16:14:53 +0900

    S05P12B308-20 [feat]: 로그인 기능 구현(Frontend)

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,11 +1,14 @@
 <template>
 <h1>Home</h1>
+<Login/>
 </template>
 
 <script>
+import Login from '@/components/home/Login'
 export default {
   name: 'Home',
   components: {
+    Login
   }
 }
 </script>
