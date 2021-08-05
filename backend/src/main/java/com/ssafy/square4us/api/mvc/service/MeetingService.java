9a3d6e2b5c77208f4915da35f5923d2cabfd8d30
윤이진 <483759@naver.com>
2021-08-06 00:41:00 +0900

    #S05P13B308-59 [refactor]: 기존 Request, Response 클래스들 삭제

--- backend/src/main/java/com/ssafy/square4us/api/mvc/service/MeetingService.java
+++ backend/src/main/java/com/ssafy/square4us/api/mvc/service/MeetingService.java
@@ -1,7 +1,6 @@
 package com.ssafy.square4us.api.mvc.service;
 
 import com.ssafy.square4us.api.mvc.model.entity.Meeting;
-import com.ssafy.square4us.api.request.MeetingCreatePostReq;
 
 import java.util.List;
 
