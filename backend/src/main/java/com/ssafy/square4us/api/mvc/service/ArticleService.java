8f860603a4b4e4447450379873e8d98eaa0c6757
최경운 <mengno5314@naver.com>
2021-08-10 21:45:30 +0900

    #S05P13B308-85 [feat]: 스터디 및 게시글 관련 파일 업로드 삭제 기능 구현(Backend)

--- backend/src/main/java/com/ssafy/square4us/api/mvc/service/ArticleService.java
+++ backend/src/main/java/com/ssafy/square4us/api/mvc/service/ArticleService.java
@@ -3,13 +3,14 @@ package com.ssafy.square4us.api.mvc.service;
 import com.ssafy.square4us.api.mvc.model.dto.ArticleDTO;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface ArticleService {
-    ArticleDTO createArticle(Long studyId, Long MemberId, ArticleDTO.CreatePostReq req);
+    ArticleDTO createArticle(Long studyId, Long MemberId, ArticleDTO.CreatePostReq req, MultipartFile[] files);
     PageImpl<ArticleDTO> findStudiesWithPaging(Pageable pageable, Long studyId);
     ArticleDTO readArticle(Long articleId);
     ArticleDTO getArticle(Long articleId);
     void deleteByArticleId(Long articleId);
     void evalArticle(Long articleId, String what);
-    void updateArticle(Long articleId, ArticleDTO.CreatePostReq req);
+    void updateArticle(Long articleId, ArticleDTO.CreatePostReq req, MultipartFile[] files);
 }
