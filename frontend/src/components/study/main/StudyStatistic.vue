cac8a081cbdab30e53ae7b0bd706f89fafefbf0d
wns312 <wns312@gmail.com>
2021-08-11 13:15:55 +0900

    #S05P13B308-92 [feat]: 스터디 메뉴 컴포넌트 생성(Frontend)

---
+++ frontend/src/components/study/main/StudyStatistic.vue
@@ -0,0 +1,16 @@
+<template>
+  <header id='section-title'>
+    <div>스터디 통계</div>
+  </header>
+
+</template>
+
+<script>
+export default {
+  name: 'StudyStatistic'
+}
+</script>
+
+<style>
+
+</style>
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -6,7 +6,9 @@ export default createStore({
   state: {
     isLogin: false,
     user: {},
-    myStudies: []
+    myStudies: [],
+    myMeetings: [],
+    activeStudyNav : 0
   },
   mutations: {
     LOGIN : function (state) {
@@ -15,12 +17,19 @@ export default createStore({
     LOGOUT : function (state) {
       state.isLogin = false
       state.user = {}
+      state.myStudies = []
     },
     SET_USER: function (state, payload) {
       state.user = payload
     },
-    SET_STUDIES: function (state, payload) {
+    SET_MY_STUDIES: function (state, payload) {
       state.myStudies = payload
+    },
+    SET_MEETINGS: function (state, payload) {
+      state.myMeetings = payload
+    },
+    SET_STUDY_ACTIVE : function (state, payload) {
+      state.activeStudyNav = payload
     }
   },
   actions: {
@@ -39,6 +48,7 @@ export default createStore({
         return
       }
       localStorage.setItem('JWT', response.data.data.accessToken)
+      axios.defaults.headers.common['Authorization'] = `Bearer ${response.data.data.accessToken}`
       console.log("로그인 성공", localStorage);
       context.commit('LOGIN')
       router.push({name: 'StudyList'})
@@ -76,6 +86,34 @@ export default createStore({
       const studyId = response.data.data.id
       router.push({path: `/study/${studyId}`})
     },
+    createMeeting: async function (context, data) {
+      const response = await axios({
+        method: 'POST',
+        url: `/study/${data.studyId}/meeting/${data.maximum}`,
+      }).catch((err)=>{
+        console.log(err.response);
+      })
+      if (!response) {
+        alert('생성 실패')
+        console.log(response);
+        return
+      }
+      console.log(response);
+    },
+    getMeetings: async function (context, studyId) {
+      const response = await axios({
+        method: 'GET',
+        url: `/study/${studyId}/meeting`,
+      }).catch((err)=>{
+        console.log(err.response);
+      })
+      if (!response) {
+        alert('목록 조회 실패')
+        console.log(response);
+        return
+      }
+      context.commit('SET_MEETINGS', response.data.data.meetings)
+    },
     getUser : async function (context) {
       const response = await axios({
         method: "GET",
@@ -85,9 +123,8 @@ export default createStore({
         localStorage.removeItem('JWT');
       })
       if (!response) return false
+      await context.commit('SET_USER', response.data.data)
       context.commit('LOGIN')
-      // context.commit('SET_USER', response.data)
-      console.log("유저 정보 받아옴", response);
       return true
     },
     getMyStudies: async function (context) {
@@ -101,7 +138,22 @@ export default createStore({
         alert("내 스터디 목록을 받아오지 못했습니다")
         return
       }
-      context.commit('SET_STUDIES', response.data.data.studyList)
+      context.commit('SET_MY_STUDIES', response.data.data.studyList)
+    },
+    joinStudy: async function (context, studyId) {
+      ///api/study/{studyId}
+      const response = await axios({
+        method: "POST",
+        url: `/study/${studyId}`,
+      }).catch((err)=>{
+        console.log(err.response);
+      })
+      if (!response) {
+        alert("내 스터디 목록을 받아오지 못했습니다")
+        return
+      }
+      alert('가입 신청 성공')
+      console.log(response.data.data);
     }
   },
   modules: {},
