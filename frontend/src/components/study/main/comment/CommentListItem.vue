b28f1c9bb9c47d3269cb9ccfd5735645d3dbcac5
윤이진 <483759@naver.com>
2021-08-14 23:58:36 +0900

    #S05P13B308-120 [feat]: 댓글 작성 및 목록 불러오기(Front)

---
+++ frontend/src/components/study/main/comment/CommentListItem.vue
@@ -0,0 +1,24 @@
+<template lang="">
+  <div>
+    <p>{{comment.content}}</p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'CommentListItem',
+  props: {
+    comment: {
+      type: Object,
+      required: true
+    }
+  },
+  setup(props) {
+    return {
+      props
+    }
+  }
+}
+</script>
+<style lang="">
+  
+</style>
